9d9bef368b7c556d12b70382c34a453bbb0d3d81
BelacEr <caleblopezmorgado2007@gmail.com>
2025-09-20 22:09:37 -0600

    Add try-except statements for built-in exceptions. Add more file types and modified some comments.

--- file_organizer_cli.py
+++ file_organizer_cli.py
@@ -3,38 +3,45 @@ import os
 import shutil
 
 file_type = {
-    "image": ["jpg", "png", "jpeg"],
-    "video": ["mp4", "mov", "avi"],
-    "document": ["pdf", "docx", "txt"],
-    "executable": ["exe", "msi"],
+    "images": ["jpg", "png", "jpeg", "webp"],
+    "videos": ["mp4", "mov", "avi"],
+    "documents": ["pdf", "docx", "txt", "xlsx", "pptx", "md"],
+    "executables": ["exe", "msi"],
     "compressed": ["zip", "rar", "7z"],
-    "code": ["py", "java", "cpp", "html", "css", "js"],
+    "code": ["py", "java", "cpp", "html", "css", "js", "json", "xml", "sql"],
     "other": []
 }
 
 # Ask user for path, default to current folder if they press Enter
 while True:
-    directory_path = input("Enter the directory path (or 'q' to quit): ")
+    try:
+        directory_path = input("\nEnter the directory path (or 'q' to quit): ")
+    # The program exits gracefully when the user presses Ctrl+C (KeyboardInterrupt) or Ctrl+D (EOFError).
+    except (KeyboardInterrupt, EOFError):
+        print("\nThank you for using File Organizer!")
+        sys.exit()
+
     if directory_path.lower() == 'q':
         sys.exit()
+
     if not directory_path:
         directory_path = os.getcwd()  # current folder where the .exe runs
 
     if os.path.isdir(directory_path):
         for file in os.listdir(directory_path):
-            # 1. skip directories
+            # 1. Skip directories.
             if os.path.isdir(os.path.join(directory_path, file)):
                 continue
 
-            # 2. Skip hidden/system files
+            # 2. Skip hidden/system files.
             if file.startswith('.'):
                 continue
 
-            # 3. normalize extension
+            # 3. Normalize extension.
             file_extension = os.path.splitext(file)[1][1:].lower()
             moved = False
-            # also could be root, extension = os.path.splitext(file) instead of extension os.path.splitext(file)[1]
-            # this would give both the root and the extension
+            # Also could be root, extension = os.path.splitext(file) instead of extension os.path.splitext(file)[1]
+            # This would give both the root and the extension.
 
             for category, extensions in file_type.items():
                 if file_extension in extensions:
@@ -46,9 +53,9 @@ while True:
                         os.path.join(directory_path, directory_name, file))
                     moved = True
                     break
-                    # stop once file is moved
+                    # Stop once file is moved.
 
-            # 5. Move to "OTHER" if no match
+            # 5. Move to "OTHER" if no match.
             if not moved:
                 directory_name = "OTHER"
                 os.makedirs(os.path.join(directory_path, directory_name),
@@ -56,8 +63,13 @@ while True:
                 shutil.move(os.path.join(directory_path, file),
                             os.path.join(directory_path, directory_name, file))
         print("\nAll files have been organized!")
-        input("Press Enter to exit...")
-        break  # done organizing, exit loop
+
+        try:
+            input("Press Enter to exit...")
+        except (KeyboardInterrupt, EOFError):
+            print("\nThank you for using File Organizer!")
+
+        break  # Done organizing, exit loop.
     else:
-        print("Invalid directory path. Please enter a valid directory path.")
+        print("\nInvalid directory path. Please enter a valid directory path.")
         continue
